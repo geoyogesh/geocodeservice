ead3e2c76947f1034838b339cb0dfe9d7edb55d8
Yogesh Dhanapal <geoyogesh@gmail.com>
2012-10-08 22:36:12 +0530

    project added

---
+++ GISServer/GeocodeService/Modules/CatalogModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeocodeService.Modules
+{
+    public class CatalogModule:Nancy.NancyModule
+    {
+        public CatalogModule()
+        {
+            Get["/"] = _ =>
+                {
+                    return "Hello world";
+                };
+        }
+    }
+}
